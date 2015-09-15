7af26690f894b473fe89dacf04fd69790f754111
lirla@gmail.com <firexable@gmail.com>
2015-09-15 17:57:46 +0300

    Win condition

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -19,17 +19,15 @@ var APP = React.createClass({
 			emptyCell:index,
 			positions:pos
 		});
-		console.log(_.xor([1,2,3],[3,2,1]));
-		if(!_.xor(this.state.positions,_.range(1,17))){
+		var win = _.every(this.state.positions,function(value,index,array){
+			return index === 0 || parseInt(array[index - 1]) <= parseInt(value)
+		});
+		if(win){
 			 console.log('U Win!!!');
 		}
 	},
 	componentWillMount(){
 		this.setState({emptyCell:this.state.positions.indexOf(16)});
-	},
-	componentWillUpdate(){
-		window.alert('Now Update!!');
-		//try to do some animations
 	},
 	render() {
 		var that = this;
