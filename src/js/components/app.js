185d703e68f87f11be1e5c44a4c7a3b058560f5c
lirla@gmail.com <firexable@gmail.com>
2016-02-12 15:53:19 +0300

    Remove react-motions, using only css animations;

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import {shuffle, range, every,} from 'lodash'
-import {Motion, spring} from 'react-motion'
 
 const layout = range(0, 16).map(n => {
     const row = Math.floor(n / 4);
@@ -14,19 +13,16 @@ export default class APP extends Component {
             positions: shuffle(range(0, 16))
         }
     }
-
     updatePosition(index) {
-        let arr = this.state.positions;
-        let emptyIndex = arr.indexOf(0);
-        let targetIndex = arr.indexOf(index);
+        let {positions} = this.state;
+        let emptyIndex = positions.indexOf(0);
+        let targetIndex = positions.indexOf(index);
         const dif = Math.abs(targetIndex - emptyIndex);
         if (dif == 1 || dif == 4) {
-            arr[emptyIndex] = index;
-            arr[targetIndex] = 0;
-            this.setState({
-                positions: arr
-            });
-            let win = every(arr, (value, index, array)=> {
+            positions[emptyIndex] = index;
+            positions[targetIndex] = 0;
+            this.setState({positions});
+            let win = every(positions, (value, index, array)=> {
                 value = value || 16;
                 return index === 0 || parseInt(array[index - 1]) <= parseInt(value)
             });
@@ -35,25 +31,15 @@ export default class APP extends Component {
             }
         }
     }
-
     render() {
         return (<div className="game">
             {this.state.positions.map((i, key)=> {
-                let cellClass = (key == 0) ? 'empty cell' : "cell";
-                let x;
-                let y;
-                [x,y] = layout[this.state.positions.indexOf(key)];
-                let style = {
-                    tX: spring(x),
-                    tY: spring(y)
-                };
-                return <Motion style={style} key={key}>
-                    {({ tX, tY }) =>
-                        <div className={cellClass}
+                let cellClass = key ? "cell":'empty cell';
+                let [x,y] = layout[this.state.positions.indexOf(key)];
+                return <div className={cellClass}
                              onClick={this.updatePosition.bind(this, key)}
-                             style={{transform: `translate3d(${tX}px,${tY}px,0) scale(1.1)`}}>{key}</div>
-                    }
-                </Motion>
+                             style={{transform: `translate3d(${x}px,${y}px,0) scale(1.1)`}}>{key}</div>
+
             })}
         </div>)
     }
